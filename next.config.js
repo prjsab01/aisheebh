629c45e95e9227081fd29a29808a0a8586dc7530
Bhattacharya <aishee.bhattacharya@accenture.com>
2025-12-21 21:38:39 +0530

    Remove PWA to fix Cloudflare Pages size limit

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development'
-})
-
 const nextConfig = {
   images: {
     domains: ['firebasestorage.googleapis.com'],
   },
 }
 
-module.exports = withPWA(nextConfig)
+module.exports = nextConfig
